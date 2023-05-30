988ea10bf542e2ae7178eeaa699213114bd5c07a
Srijan Ray <srijanray12@gmail.com>
2023-05-29 23:06:34 -0400

    Edited the HomeProjectCard Component to include just a picture and a title

--- src/components/HomeProjectCard.jsx
+++ src/components/HomeProjectCard.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+
 export default function ProjectCard() {
     return (
-      <button class="h-8 w-8 bg-slate-400">One Project</button>
+      <a href={props.href}>
+        <h2>
+          {props.title}
+          <span>&rarr;</span>
+        </h2>
+        <p>
+          {props.body}
+        </p>
+      </a>
     );
   }
